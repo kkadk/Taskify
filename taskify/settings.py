75736fbab8a629e4b0f71b47e3196b6e405250bd
kkadk <125792968+kkadk@users.noreply.github.com>
2025-07-04 21:01:40 +0545

    add debug in environment variables

--- taskify/settings.py
+++ taskify/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'your-default-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
